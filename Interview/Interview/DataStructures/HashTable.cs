e3f7573cc36d2868cee340270dc9d4daac21618c
Yu Chen <13283070@qq.com>
2017-02-15 00:31:49 +0800

    initialize leetcode repo with one method

---
+++ Interview/Interview/DataStructures/HashTable.cs
@@ -0,0 +1,25 @@
+﻿namespace Interview.DataStructures
+{
+    using System.Collections.Generic;
+
+    public class HashTable
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dictionary.ContainsKey(nums[i]))
+                {
+                    return new int[] { dictionary[nums[i]], i };
+                }
+                else
+                {
+                    dictionary[target - nums[i]] = i;
+                }
+            }
+
+            return new int[] { -1, -1 };
+        }
+    }
+}
---
+++ Interview/UnitTests/DataStructures/TwoSumTests.cs
@@ -0,0 +1,56 @@
+﻿namespace UnitTests.DataStructures
+{
+    using Interview.DataStructures;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TwoSumTests
+    {
+        HashTable hashTable = new HashTable();
+
+        [TestMethod]
+        public void TwoSum_ExactlyOneSolution_ReturnValidIndices()
+        {
+            int[] nums = new int[] { -1, 2, 4, 8 };
+            var indices = hashTable.TwoSum(nums, 6);
+
+            CollectionAssert.AreEqual(new int[] { 1, 2 }, indices);
+        }
+
+        [TestMethod]
+        public void TwoSum_MoreThanOneSolution_ReturnFirstValidIndices()
+        {
+            int[] nums = new int[] { -1, 2, 4, 8 -2};
+            var indices = hashTable.TwoSum(nums, 6);
+
+            CollectionAssert.AreEqual(new int[] { 1, 2 }, indices);
+        }
+
+        [TestMethod]
+        public void TwoSum_NoSolution_ReturnNegativeIndices()
+        {
+            int[] nums = new int[] { -1, 2, 4, 8 };
+            var indices = hashTable.TwoSum(nums, 8);
+
+            CollectionAssert.AreEqual(new int[] { -1, -1 }, indices);
+        }
+
+        [TestMethod]
+        public void TwoSum_NoSolutionWithOneNumberInput_ReturnNegativeIndices()
+        {
+            int[] nums = new int[] { -1 };
+            var indices = hashTable.TwoSum(nums, 8);
+
+            CollectionAssert.AreEqual(new int[] { -1, -1 }, indices);
+        }
+
+        [TestMethod]
+        public void TwoSum_NoSolutionWithNoNumberInput_ReturnNegativeIndices()
+        {
+            int[] nums = new int[] {};
+            var indices = hashTable.TwoSum(nums, 8);
+
+            CollectionAssert.AreEqual(new int[] { -1, -1 }, indices);
+        }
+    }
+}
