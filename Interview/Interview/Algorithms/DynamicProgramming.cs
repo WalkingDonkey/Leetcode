3eabf4a5f972067391636550f77c3bdbd1a43c30
ychenu <jsnt_cy@hotmail.com>
2017-04-16 18:37:10 +0800

    update dp

--- Interview/Interview/Algorithms/DynamicProgramming.cs
+++ Interview/Interview/Algorithms/DynamicProgramming.cs
@@ -1,6 +1,7 @@
 ﻿namespace Interview.Algorithms
 {
     using System;
+    using System.Linq;
 
     public class DynamicProgramming
     {
@@ -12,18 +13,54 @@
                 throw new ArgumentException("n should not be less than zero.");
             }
 
-            var predefinedCount = new int[] { 0, 1, 2, 1, 2, 1 };
-            if (amount <= 5)
+            var count = new int[amount + 1];
+            count[0] = 0;
+
+            for (var i = 1; i <= amount; i++)
             {
-                return predefinedCount[amount];
+                if(i < 3)
+                {
+                    count[i] = count[i - 1] + 1;
+                }
+                if (i >= 5)
+                {
+                    count[i] = Math.Min(Math.Min(count[i - 1], count[i - 3]), count[i - 5]) + 1;
+                }
+                else if (i >= 3)
+                {
+                    count[i] = Math.Min(count[i - 1], count[i - 3]) + 1;
+                }
+                else
+                {
+                    count[i] = count[i - 1] + 1;
+                }
             }
 
-            return Math.Min(Math.Min(CoinChange(amount - 1) + 1, CoinChange(amount - 3) + 1), CoinChange(amount - 5) + 1);
+            return count[amount];
         }
 
         public int CoinChange(int[] coins, int amount)
         {
-            return 0;
+            if (amount < 0)
+            {
+                throw new ArgumentException("n should not be less than zero.");
+            }
+
+            var count = Enumerable.Repeat(amount + 1, amount + 1).ToArray();
+            count[0] = 0;
+
+            for (var i = 1; i <= amount; i++)
+            {
+                for (var j = 0; j < coins.Length; j++)
+                {
+                    if (i >= coins[j])
+                    {
+                        count[i] = Math.Min(count[i], count[i - coins[j]] + 1);
+                    }
+                }
+            }
+
+            return count[amount] > amount ? -1 : count[amount];
         }
     }
 }
