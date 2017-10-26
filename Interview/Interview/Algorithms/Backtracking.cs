5d8e1bf0fc537028161d22e8103a67d6fdda698e
walkingdonkey <Administrator@cn-ychenu-w10.fareast.corp.microsoft.com>
2017-10-27 07:35:56 +0800

    generate parenthesis

---
+++ Interview/Interview/Algorithms/Backtracking.cs
@@ -0,0 +1,47 @@
+﻿namespace Interview.Algorithms
+{
+    using System.Collections.Generic;
+
+    public class Backtracking
+    {
+        // Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+        // For example, given n = 3, a solution set is:
+        //        [
+        //          "((()))",
+        //          "(()())",
+        //          "(())()",
+        //          "()(())",
+        //          "()()()"
+        //]
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var parenthesisSet = new List<string>();
+            GenerateParenthesisCore(n, 0, 0, string.Empty, parenthesisSet);
+            return parenthesisSet;
+        }
+
+        private void GenerateParenthesisCore(int n, int left, int right, string s, IList<string> parenthesisSet)
+        {
+            if (left < right)
+            {
+                return;
+            }
+
+            if (left == n && right == n)
+            {
+                parenthesisSet.Add(s);
+                return;
+            }
+
+            if (left < n)
+            {
+                GenerateParenthesisCore(n, left + 1, right, s + "(", parenthesisSet);
+            }
+
+            if (right < n)
+            {
+                GenerateParenthesisCore(n, left, right + 1, s + ")", parenthesisSet);
+            }
+        }
+    }
+}
---
+++ Interview/UnitTests/Algorithms/GenerateParenthesisTests.cs
@@ -0,0 +1,18 @@
+﻿namespace UnitTests.Algorithms
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Interview.Algorithms;
+
+    [TestClass]
+    public class GenerateParenthesisTests
+    {
+        Backtracking bt = new Backtracking();
+
+        [TestMethod]
+        public void GenerateParenthesis_ValidValue_Success()
+        {
+            var actual = bt.GenerateParenthesis(3);
+        }
+    }
+}
