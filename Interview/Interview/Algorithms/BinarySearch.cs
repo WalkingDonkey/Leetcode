15b0d0d38f48752f2665c448b0a2b57ba0df68d6
walkingdonkey <Administrator@cn-ychenu-w10.fareast.corp.microsoft.com>
2017-10-28 17:54:45 +0800

    add code

--- Interview/Interview/Algorithms/BinarySearch.cs
+++ Interview/Interview/Algorithms/BinarySearch.cs
@@ -1,7 +1,7 @@
-﻿using System;
-
-namespace Interview.Algorithms
+﻿namespace Interview.Algorithms
 {
+    using System;
+
     public class BinarySearch
     {
         // You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
